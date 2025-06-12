4776690b176027379009c877fb791f7e45527329
Adam Petty <adam@SHA-MacbookAir-1Silver.local>
2025-06-11 22:13:31 -0700

    everything working with new jwtauth

--- routes/secure.js
+++ routes/secure.js
@@ -8,4 +8,10 @@ router.get('/secure/createEventForm.html', authenticateJWT, (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'private', 'createEventForm.html'));
 });
 
+// routes/secure.js
+router.get('/secure/manageUsersGroups.html', authenticateJWT, (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'private', 'manageUsersGroups.html'));
+});
+
+
 module.exports = router;
